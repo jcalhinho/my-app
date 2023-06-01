6ea38436d6574cc89ca92ddd64e33fb096c35898
jcalhinho <jc.alhinho@nanocorp.fr>
2023-06-01 23:06:32 +0200

    comm

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import "../index.css";
 
-import { motion } from "framer-motion";
-import pic from "../01-MR PAON/MR PAON_01.png";
-import pic1 from "../01-MR PAON/MR PAON_02.png";
-import pic2 from "../01-MR PAON/MR PAON_03.png";
-import pic3 from "../01-MR PAON/MR PAON_04.png";
-import pic4 from "../01-MR PAON/MR PAON_05.png";
-import pic5 from "../01-MR PAON/MR PAON_06.png";
+// import { motion } from "framer-motion";
+// import pic from "../01-MR PAON/MR PAON_01.png";
+// import pic1 from "../01-MR PAON/MR PAON_02.png";
+// import pic2 from "../01-MR PAON/MR PAON_03.png";
+// import pic3 from "../01-MR PAON/MR PAON_04.png";
+// import pic4 from "../01-MR PAON/MR PAON_05.png";
+// import pic5 from "../01-MR PAON/MR PAON_06.png";
 import {Divider} from "@mui/material";
 
 
